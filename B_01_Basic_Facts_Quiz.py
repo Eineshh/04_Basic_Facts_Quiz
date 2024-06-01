cacc1c6ce73e42ba272acf2882be8d75693c582b
Eineshh <142182572+Eineshh@users.noreply.github.com>
2024-06-01 23:13:12 +1200

    Completed Quiz With Updated Quiz History

--- B_01_Basic_Facts_Quiz.py
+++ B_01_Basic_Facts_Quiz.py
@@ -7,16 +7,14 @@ def yes_no(question):
         response = input(question).lower()
         if response in ["yes", "y"]:
             return "yes"
-
         elif response in ["no", "n"]:
             return "no"
-
         else:
             print("Please enter Yes or No!")
             print()
 
 
-# Display's the instructions
+# Displays the instructions
 def instructions():
     print('''
 🌟🌟 Instructions 🌟🌟
@@ -29,30 +27,28 @@ Welcome to my quiz 😊, it seems like it's your first time playing...
 
 
 🎗️ ️ To begin with, choose the desired number of questions or press <enter> for 
-   ️  endless questions,
+   ️ endless questions,
 
 🎗️ Next, choose the difficulty between 1 - 3 (Easy (1) / Medium (2) / Hard (3) as it 
-    will determine the complexity of the questions given,
+   will determine the complexity of the questions given,
 
 🎗️ If you're wanting to challenge yourself, I highly suggest you to pick difficulty 3!,
 
 🎗️ Your goal is to answer the questions correctly within the three attempts given 
-    or face getting the question wrong,
+   or face getting the question wrong,
 
 🎗️ You may choose to exit the quiz at any time by entering "xxx",
 
 🎗️ Once you've completed the quiz, your results are displayed with the option of 
-    viewing the history of your questions too.
-
+   viewing the question history too!.
 
 🌟 Have Fun! :) 🌟''')
 
 
-# Check's if the user has entered a valid integer
+# Checks for an integer between the inclusive numbers
 def int_check(question, low=None, high=None, exit_code="xxx"):
 
     # Sets up the error message
-
     if low is None and high is None:
         error = "Please Enter an Integer"
 
@@ -85,7 +81,7 @@ def int_check(question, low=None, high=None, exit_code="xxx"):
             print(error)
 
 
-# Generates the questions based on it's difficulty
+# Generates the questions based on the difficulty chosen
 def question_generator(difficulty):
 
     # Initializes the ranges of difficulty
@@ -99,30 +95,30 @@ def question_generator(difficulty):
         num_1 = random.randint(1, 10)
         num_2 = random.randint(1, 10)
 
-    # Lists the possible math operations
+    # Lists the possible math operations in the questions
     math_operations = ["addition", "subtraction", "multiplication", "division"]
     operation = random.choice(math_operations)
 
     # Generates the questions & answers for the math operations involved
     if operation == "subtraction":
         num_1 = num_1 + num_2
-        question = f"What's {num_1} - {num_2}?: "
+        question = f"What's {num_1} - {num_2}? "
         answer = num_1 - num_2
     elif operation == "addition":
-        question = f"What's {num_1} + {num_2}?: "
+        question = f"What's {num_1} + {num_2}? "
         answer = num_1 + num_2
     elif operation == "multiplication":
-        question = f"What's {num_1} x {num_2}?: "
+        question = f"What's {num_1} x {num_2}? "
         answer = num_1 * num_2
     else:
         num_1 = num_1 * num_2
-        question = f"What's {num_1} ÷ {num_2}?: "
+        question = f"What's {num_1} ÷ {num_2}? "
         answer = num_1 // num_2
 
     return question, answer
 
 
-# Generates the question numbers and attempts
+# Prints the question's answers and attempts used
 def num_question(num, num_questions, difficulty, question_number):
     # Displays the question numbers
     if num_questions is None:
@@ -144,13 +140,14 @@ def num_question(num, num_questions, difficulty, question_number):
         # Collects the users answer
         users_answer = int_check(question, low=1)
 
-        # Quits the quiz if exit code is entered
+        # Quits the quiz if the exit code is entered
         if users_answer == "xxx":
-            return "exit", answer
+            return "exit", answer, question
 
+        # Prints if the question has been answered correctly, or incorrectly
         if users_answer == answer:
             print(f"🎉 You got the answer right, it was {answer}")
-            return "correct", answer
+            return "correct", answer, question
         elif users_answer in wrong_answers:
             print("💔 You already provided this incorrect answer")
         else:
@@ -159,21 +156,21 @@ def num_question(num, num_questions, difficulty, question_number):
             if attempts < 3:
                 print("💔 You got the answer wrong. Please try again!")
             else:
-                print(f"😿 Sorry, You ran out of attempts. The correct answer was {answer}.")
-                return "wrong", answer
+                print(f"😿 Sorry, You ran out of attempts. The correct answer was {answer}")
+                return "wrong", answer, question
 
 
-# Generates the answer and prints quiz history
-def answer_checker(num_questions):
+# Generates the quiz history
+def history_generator(num_questions):
 
-    # Initialize variables
+    # Initialize the variables
     total_won = 0
     total_lost = 0
     quiz_history = []
     question_number = 0
 
     # Asks the user for the desired difficulty
-    difficulty = int_check("Choose the difficulty (Easy (1) / Medium (2) / Hard (3): ", low=1, high=3)
+    desired_difficulty = int_check("Choose the difficulty (Easy (1) / Medium (2) / Hard (3): ", low=1, high=3)
 
     if num_questions is not None:
         num_range = range(1, num_questions + 1)
@@ -184,7 +181,7 @@ def answer_checker(num_questions):
         if num_questions is None:
             question_number += 1
 
-        result, answer = num_question(num, num_questions, difficulty, question_number)
+        result, answer, question = num_question(num, num_questions, desired_difficulty, question_number)
 
         if result == "exit":
             print("You've Ended the Quiz!")
@@ -193,20 +190,20 @@ def answer_checker(num_questions):
         if result == "correct":
             total_won += 1
             quiz_history.append(
-                f"Question {num if num_questions else question_number}: 🎉 You got the answer right!, "
-                f"(The Answer:  {answer:<3})  ✅")
+                f"Question {num if num_questions else question_number}: {question:<18} ({answer:>3}) - 🎉 You got the "
+                f"answer right! ✅")
 
         elif result == "wrong":
             total_lost += 1
             quiz_history.append(
-                f"Question {num if num_questions else question_number}: 💔 You ran out of attempts,    "
-                f"(The Answer: {answer:<3})  ❌")
+                f"Question {num if num_questions else question_number}: {question:<18} ({answer:>3}) - 💔 You ran out "
+                f"of attempts   ❌")
 
     # Calculates the quiz results
     total_questions = total_won + total_lost
     quiz_statistics(total_won, total_lost, total_questions)
 
-    # Prints the quiz history
+    # Checks if the user wants the quiz history, and prints it out
     print()
     history_feedback = yes_no("Would you like to view the Quiz History?: ")
     if history_feedback == "yes":
@@ -215,9 +212,10 @@ def answer_checker(num_questions):
             print(item)
 
 
-# Generates the quiz results
+# Generates the quiz statistics
 def quiz_statistics(total_won, total_lost, total_questions):
 
+    # Calculate's the average percentage of the questions
     correct_percentage = (total_won / total_questions) * 100 if total_questions > 0 else 0
     wrong_percentage = (total_lost / total_questions) * 100 if total_questions > 0 else 0
 
@@ -243,22 +241,25 @@ def quiz_statistics(total_won, total_lost, total_questions):
 # Displays the title
 print("✨ Welcome to Einesh's Basic Facts Quiz! ✨")
 
-# Loop for testing purposes
+# Asks if the user would like to see the instructions using the Yes & No function
 print()
 want_instructions = yes_no("Would you like to read the instructions?: ")
 
-# Displays the instructions
+# Displays the instructions if "Yes" has been returned
 if want_instructions == "yes":
     instructions()
 
-# Asks the user for the desired questions
+# Asks the user for the desired amount of questions
 print()
 number_questions = int_check("How many questions would you like? (<enter> for Endless Questions): ",
                              low=1, exit_code="xxx")
+
+# Informs the user that the exit code has been entered
 if number_questions == "xxx":
     print("You Quit!")
 else:
-    answer_checker(number_questions)
+    history_generator(number_questions)
 
+# Prints the quiz has ended
 print()
 print("😻 Thank You for Playing My Quiz! 😻")
